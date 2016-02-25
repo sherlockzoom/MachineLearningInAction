fab170cd62eae472f31d6d84eec0e4e2e324a9ce
sherlockzoom <seezhouyunli@gmail.com>
2016-02-25 17:24:14 +0800

    add apriori

--- chapter11/apriori.py
+++ chapter11/apriori.py
@@ -33,3 +33,29 @@ def scanD(D, Ck, minSupport):   # 数据集D，候选集合Ck，最小支持度m
             retList.insert(0,key)
         supportData[key]= support
     return retList, supportData
+
+def aprioriGen(Lk, k): #creates Ck
+    retList = []
+    lenLk = len(Lk)
+    for i in range(lenLk):
+        for j in range(i+1, lenLk):
+            L1 = list(Lk[i])[:k-2]; L2 = list(Lk[j])[:k-2]
+            L1.sort(); L2.sort()
+            if L1==L2:
+                retList.append(Lk[i]|Lk[j])
+    return retList
+
+
+def apriori(dataSet, minSupport=0.5):
+    C1 = createC1(dataSet)
+    D = map(set, dataSet)
+    L1, supportData = scanD(D, C1, minSupport)
+    L = [L1]
+    k = 2
+    while(len(L[k-2]) > 0):
+        Ck = aprioriGen(L[k-2], k)
+        Lk, supK = scanD(D, Ck, minSupport)
+        supportData.update(supK)
+        L.append(Lk)
+        k += 1
+    return L, supportData
